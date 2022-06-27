b8adafc0909b39b4e9cc514d259de058ad1afa16
akira7891 <akiraakira462@gmail.com>
2022-06-28 06:51:19 +0900

    トップページから個別ページへ移動できる機能を追加

--- resources/views/data/index.blade.php
+++ resources/views/data/index.blade.php
@@ -1,3 +1,18 @@
-@foreach($datas as $data)
-データのアイデー{{$data->id}}　データのタイトル{{$data->title}}　データの内容{{$data->content}}　データの作成日{{$data->created_at}}　データの更新日{{$data->updated_at}}<br>
-@endforeach
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>データ一覧</h1>
+    @foreach($datas as $data)
+    <a href="/{{$data->id}}">{{$data->title}}</a>
+    <br>
+    @endforeach
+</body>
+</html>
